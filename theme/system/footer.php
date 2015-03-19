9ce8ea05677b09d2a1703733b402ade940a33c8a
Tobias F. Petersen <tobsf@hotmail.com>
2015-03-19 13:41:38 +0100

    first commit strap

---
+++ theme/system/footer.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<h4><?php print t('Odense, Digital Formidling © 2015 All rights reserved .');?></h4>
+			</div>
+			<div class="col-md-6 text-right">
+				<h4><?php print t('<a href="https://www.odensebib.dk">Udviklet af Digital Formidling / Odense Centralbibliotek</a>');?></h4>
+			</div>
+		</div>
+	</div>
+</footer>
---
+++ theme/system/frontpage.php
@@ -0,0 +1,18 @@
+<div class="frontpage">
+	<div class="container">
+	 <section class="row">
+	    <div class="col-md-12">
+	     	<div class="col-md-4 pull-right front-container">
+	    		<h1><?php print t('BRUGER LOGIN');?></h1>
+	    		<?php print drupal_render(drupal_get_form('user_login')); ?>
+	     	</div>
+	    	<div class="col-md-8 pull-left front-container">
+	    		<h1><?php print t('Velkommen til Drone BKF');?></h1>
+	    		<h2><?php print t('Register for certificerede drone flyvninger i Danmark');?></h2>
+	    		<h2><?php print t('Log dig ind i boksen til højre for eller,');?> </h2><br/>
+	    		<a href="/skriv-mig-op" class="btn btn-default btn-linkz"><?php print t('SKRIV DIG OP HER');?></a>
+	    	</div>
+	    </div>
+	  </section>
+	</div>
+</div>
